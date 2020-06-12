a48fcdaa4b7cc598875f0a5257abecb7d0f01849
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-06-12 15:20:19 +0100

    initial commit for ui-ux

---
+++ src/components/layouts/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./main.css"
+
+const Main = () => {
+    return (
+        <React.Fragment>
+            <div className="main">
+                Main
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Main
---
+++ src/components/layouts/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './sidebar.css'
+
+const Sidebar = () => {
+
+    return (
+        <React.Fragment>
+            <div className="sidebar" id="sidebar">
+                <div className="center">
+                    <div className="logo" />
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Portfolio</li>
+                        <li>Place bookings</li>
+                    </ul>
+                </div>
+
+                <div className="fixed-bottom">
+                    <button>Fcebook</button>
+                    <button>Instagrram</button>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Sidebar
