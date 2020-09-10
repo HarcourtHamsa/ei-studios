20194f941b665a94de1b4996dd35a5583ffe53df
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-09-10 08:01:20 +0100

    deploying

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import Sidebar from "./components/layouts/Sidebar"
-import Main from "./components/layouts/Main"
-import Loader from "./components/layouts/Loader"
-import FloatingBtn from "./components/layouts/FloatingBtn"
+import React from 'react';
+import Main from './components/layouts/Main';
 import './App.css';
 
 function App () {
-  const [loading, setLoading] = useState (true);
-
-  useEffect (() => {
-    setTimeout (() => {
-      setLoading (false);
-    }, 1000 * 3);
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
-
   return (
     <div className="App">
-      <div className="side">
-        <Sidebar />
-      </div>
       <div className="main">
         <Main />
-        <FloatingBtn />
       </div>
     </div>
   );
--- src/components/layouts/Main.js
+++ src/components/layouts/Main.js
@@ -2,16 +2,60 @@ import React from 'react';
 import Gallery from 'react-photo-gallery';
 import {photos} from '../../photo';
 
+import {FaFacebook, FaTwitter, FaInstagramSquare} from 'react-icons/fa';
+
 import './main.css';
 
 const Main = () => {
   return (
     <div>
-      <div className="center-text">
-        <h3>EI STUDIOS</h3>
-      </div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-white mb-3 p-3 small">
+        <a class="navbar-brand" href="/">Eddies Illution Photography</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0 float-right">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">About</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/">Team</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/">Portfolio</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
       <Gallery photos={photos} />
+
+      <div className="icons text-center mt-1">
+        <FaFacebook size="20" /><br />
+        <FaInstagramSquare size="20" /><br />
+        <FaTwitter size="20" />
+      </div>
     </div>
   );
 };
